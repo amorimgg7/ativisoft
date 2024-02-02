80cbb16b60c2d0a4bd6039ddb93e99e5f9d10ee6
Amorimgg7 <amorimgg7@gmail.com>
2024-02-02 18:40:21 -0300

    atividade LIBERADO migrada para outra tela

--- pages/md_assistencia/index.php
+++ pages/md_assistencia/index.php
@@ -224,8 +224,7 @@
             ?>
 
             <?php //LIBERADO
-              //"SELECT marca_patrimonio, modelo_patrimonio, COUNT(*) AS total FROM tb_patrimonio WHERE tipo_patrimonio = 'Impressora' GROUP BY marca_patrimonio, modelo_patrimonio";
-              //$sql_servico = "SELECT * FROM tb_servico WHERE status_servico = 0";
+            
               $sql_servico = "SELECT * FROM tb_servico WHERE status_servico = 2 
                 ORDER BY 
                 CASE 
@@ -241,71 +240,15 @@
                 echo '<div class="card" '.$_SESSION['c_card'].'>';
                 echo '<div class="card-body">';
                 echo '<h4>LIBERADO</h4>';
-                
+              
                 echo '<div class="collapse table-responsive" id="os_liberado">';
-                echo '<table class="table" '.$_SESSION['c_card'].'>';
-                echo '<thead>';
-                echo '<tr>';
-                echo '<th>OS</th>';
-                echo '<th>Prioridade</th>';
-                echo '<th>Prazo</th>';
-                
-                echo '</tr>';
-                echo '</thead>';
-                echo '<tbody>';
-                
-                while ( $servico = $resulta_servico->fetch_assoc()){
-                  echo '<tr>';
-                  echo '<form method="POST" action="../../pages/md_assistencia/consulta_servico.php">';
-                  echo '<td style="display: none;"><input type="tel" id="conos_servico" name="conos_servico" value="'.$servico['cd_servico'].'"></td>';
-                  echo '<td><button type="submit" class="btn btn-danger" name="btn_cd_'.$servico['cd_servico'].'" id="btn_cd_'.$servico['cd_servico'].'">'.$servico['cd_servico'].'</button></td>';
-                
-                  echo '</form>';
-                  
-
-                  if(date('Y-m-d', strtotime('+1 hour')) > date('Y-m-d', strtotime($servico['prazo_servico']))){
-                    echo '<script>document.getElementById("btn_cd_'.$servico['cd_servico'].'").className = "btn btn-danger";</script>';
-                    //$extrapolado = $extrapolado+1;
-                  }else{
-                    if(date('Y-m-d', strtotime('+1 hour')) == date('Y-m-d', strtotime($servico['prazo_servico']))){
-                    echo '<script>document.getElementById("btn_cd_'.$servico['cd_servico'].'").className = "btn btn-warning";</script>';
-                    //$parahoje = $parahoje+1;
-                    }else{
-                      if(date('Y-m-d', strtotime('+1 hour')) < date('Y-m-d', strtotime($servico['prazo_servico']))){
-                        echo '<script>document.getElementById("btn_cd_'.$servico['cd_servico'].'").className = "btn btn-success";</script>';
-                        //$noprazo = $noprazo+1;
-                      }
-                    }
-                  }                 
-                  if($servico['prioridade_servico'] == "B"){
-                    echo '<td><label class="badge badge-success">Baixa</label></td>';
-                  
-                  }
-                  if($servico['prioridade_servico'] == "M"){
-                    echo '<td><label class="badge badge-info">Média</label></td>';
-                  
-                  }
-                  if($servico['prioridade_servico'] == "A"){
-                    echo '<td><label class="badge badge-warning">Alta</label></td>';
-                  
-                  }
-                  if($servico['prioridade_servico'] == "U"){
-                    echo '<td><label class="badge badge-danger">Urgente</label></td>';
-                  }
-                  echo '<td>'.date('d/m/y', strtotime($servico['prazo_servico'])).'</td>';
-                  
-                  echo '</tr>';
-                }
-                
-                 
-                echo '</tbody>';
-                echo '</table>';
+        
+                echo '<a href="../md_assistencia/painel_01.php" class="btn btn-lg btn-block btn-outline-info">VER MAIS</a>';
                 echo '</div>';
-                echo '</li>';
                 echo '</div>';
                 echo '</div>';
                 echo '</div>';  
-              }
+              }              
             ?>
 
             <?php //RETIRADO / DEVOLVIDO
@@ -327,7 +270,7 @@
                 echo '<h4>ENTREGUE / DEVOLVIDO</h4>';
                 
                 echo '<div class="collapse table-responsive" id="os_retirado">';
-                echo '<a href="../md_assistencia/painel_01.php" class="btn btn-lg btn-danger">VER MAIS</a>';
+                echo '<a href="../md_assistencia/painel_01.php" class="btn btn-lg btn-block btn-outline-info">VER MAIS</a>';
                 echo '</div>';
                 echo '</div>';
                 echo '</div>';
@@ -356,7 +299,7 @@
                 
                 echo '<div class="collapse table-responsive" id="os_arquivado">';
                 
-                echo '<a href="../md_assistencia/painel_01.php" class="btn btn-lg btn-danger">VER MAIS</a>';
+                echo '<a href="../md_assistencia/painel_01.php" class="btn btn-lg btn-block btn-outline-info">VER MAIS</a>';
 
                 
                 echo '</div>';
