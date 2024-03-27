e400032a795cad371f17adc76072a0cb9d2d4199
Gabriel Amorim <52302612+amorimgg7@users.noreply.github.com>
2024-03-27 10:33:51 -0300

    Update conn.php
    Adicionado ao gitignore mas não obedeceu

--- classes/conn.php
+++ classes/conn.php
@@ -1,12 +1,12 @@
 
 <?php
-    $host = "localhost"; /* nome da conexão */
-    $usuario = "root"; /* nome do usuario da conexãp */
-    $senha = ""; /*senha do banco de dados caso exista */
-    $nome = "ativisoft_"; /* nome do seu banco  */
+    $host = "sv65.ifastnet14.org"; /* nome da conex���o */
+    $usuario = "assistent"; /* nome do usuario da conex���p */
+    $senha = "p)0s5C[a3R8IEr"; /*senha do banco de dados caso exista */
+    $nome = "assistent_"; /* nome do seu banco  */
     $conn = new mysqli($host, $usuario, $senha, $nome."".$_SESSION['cnpj_empresa']);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    $_SESSION['dominio'] = 'http://localhost/ativisoft/';
+    $_SESSION['dominio'] = 'https://ativisoft.com.br/';
 ?>
