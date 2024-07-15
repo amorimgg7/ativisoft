a87cf9c42f5b2d4495065c5576e6b187c87e03f8
Amorimgg7 <amorimgg7@gmail.com>
2024-07-15 18:27:58 -0300

    ...

--- pages/md_hospedagem/editar_casa.php
+++ pages/md_hospedagem/editar_casa.php
@@ -913,44 +913,24 @@
                   //echo '<h3 class="card-title" '.$_SESSION["c_card"].' >Area da automação</h3>';
                 ?>
                 
-                <?php
-                  
-                  $sql_casa = "SELECT * FROM tb_dispositivo where cd_casa_dispositivo = ".$_SESSION['cd_casa'];
-                  $resulta_casa = $conn->query($sql_casa);
-                  if ($resulta_casa->num_rows > 0){
-                    while ( $casas = $resulta_casa->fetch_assoc()){
-                      //echo '<div class="col">';
-                      echo '<div class="card text-white bg-secondary mb-3 align-items-center" style="margin: 10px; max-width: 18rem;">';
-                      echo '<div class="card-header bg-success">'.$casas['local_dispositivo'].'</div>';
-                      echo '<div class="card-body">';
-                      echo '<h5 class="card-title">'.$casas['marca_dispositivo'].' '.$casas['modelo_dispositivo'].' '.$casas['versao_dispositivo'].'</h5>';
-                      echo '<p class="card-title">IP - '.$casas['ip_dispositivo'].'</p>';
-                      echo '<p class="card-title">MAC - '.$casas['mac_dispositivo'].'</p>';
-                      echo '</div>';
-                      echo '<div class="card-footer text-muted">';
-                      //<a href="'.$_SESSION['dominio'].'/pages/filmes/index.php?cd_filme='.$casas['cd_casa'].'" title="'.$casas['titulo_casa'].'">
-                      echo '<form method="post" action="'.$_SESSION['dominio'].'/pages/md_hospedagem/editar_dispositivo.php">';
-                      echo '<input type="text" id="concd_dispositivo" name="concd_dispositivo" value="'.$casas['cd_dispositivo'].'" style="display:none;">';
-                      echo '<input class="btn btn-outline-success btn-lg btn-block" type="submit" value="Parametros">';
-                      //echo '<input class="btn btn-outline-success btn-lg btn-block" type="submit" value="#2">';
-                      echo '</form>';
-                      //echo '<a href="'.$_SESSION['dominio'].'/pages/md_hospedagem/editar_casa.php?cd_casa='.$casas['cd_casa'].'" class="btn btn-outline-success btn-lg btn-block">Parametros</a>';
-                      echo '</div>';
-                      echo '</div>';
-                      //echo '</a>';
-                      //echo '</div>';
-                    }
-                  }
-                  
-                  
-                  
-                  
-                  //echo '</div>';
-                  //echo '</div>';
-                  echo '</div>';
-                  echo '</div>';
-                  echo '</div>';
-                ?>
+
+                
+                      <script>
+                        function updateContent1() {
+                          fetch('p2.php')
+                          .then(response => response.text())
+                          .then(data => {
+                            document.getElementById('content1').innerHTML = data;
+                          })
+                          .catch(error => console.error('Erro:', error));
+                        }
+                        setInterval(updateContent1, 3000);
+                        window.onload = updateContent1;
+                      </script>
+                      <div id="content1"><h1>Carregando #1...</h1></div>
+                    
+
+
                 
               </div>
 
