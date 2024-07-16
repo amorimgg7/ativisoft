b7a50c9c23d15a850d8b86f8e05bd26bc445f766
Amorimgg7 <amorimgg7@gmail.com>
2024-07-15 22:54:33 -0300

    ....

--- pages/md_hospedagem/p2.php
+++ pages/md_hospedagem/p2.php
@@ -2,7 +2,7 @@
 <?php 
     session_start();  
     
-    require_once '../../classes/conn.php';
+    require_once '../../classes/conn_revenda.php';
 ?><!--Validar sessão aberta, se usuário está logado.-->
 
 
