a634cade576e6b39411c96a9d897827fb07fde8d
Amorimgg7 <amorimgg7@gmail.com>
2024-07-15 23:08:00 -0300

    .....

--- pages/samples/login.php
+++ pages/samples/login.php
@@ -25,6 +25,7 @@
 
 
 <body>
+  
   <div class="container-scroller">
     <div class="container-fluid page-body-wrapper full-page-wrapper">
       <div class="content-wrapper d-flex align-items-center auth px-0">
