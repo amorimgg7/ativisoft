769fa3a5fa18105c1e1062ff7c90dd9bd3461c7a
amorimgg7 <amorimgg7@gmail.com>
2024-11-17 10:54:24 -0300

    .

--- classes/conn_revenda.php
+++ classes/conn_revenda.php
@@ -1,6 +1,6 @@
 
 <?php
-    session_start();
+    //session_start();
     $_SESSION['cnpj_revenda'] = 123;
     $host = "ass_123.mysql.dbaas.com.br"; /* nome da conexão */
     $usuario = "ass_123"; /* nome do usuario da conexãp */
@@ -8,10 +8,10 @@
     $nome = "ass_".$_SESSION['cnpj_revenda']; /* nome do seu banco  */
     //$conn_base = new mysqli($host, $usuario, $senha, $nome."");
     $conn_revenda = new mysqli($host, $usuario, $senha, $nome);
-   echo "Revenda: ".$_SESSION['cnpj_revenda'];
+   //echo "Revenda: ".$_SESSION['cnpj_revenda'];
     if ($conn_revenda->connect_error) {
         die("Connection failed: " . $conn_revenda->connect_error);
     }
-    $_SESSION['dominio_revenda'] = 'http://ativisoft.tecnologia.ws/';
+    $_SESSION['dominio_revenda'] = 'http://ativisoft.com.br/';
     date_default_timezone_set('America/Sao_Paulo');
 ?>
