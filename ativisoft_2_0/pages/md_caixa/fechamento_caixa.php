bf47924a04a3b4ca6479ecd0a2f99270b306eefc
amorimgg7 <amorimgg7@gmail.com>
2025-12-09 09:36:51 -0300

    09122025_1

--- ativisoft_2_0/pages/md_caixa/fechamento_caixa.php
+++ ativisoft_2_0/pages/md_caixa/fechamento_caixa.php
@@ -1020,9 +1020,11 @@
         
                                         //echo '<button type="submit" name="fechamentoDiaFiscalCaixa" id="fechamentoDiaFiscalCaixa" class="btn btn-lg btn-block btn-warning" style="margin: 5px;"><i class="mdi mdi-file-check"></i>Fechar Dia Fiscal</button>';
 
-                                        echo $u->retPermissaoBtn('103', 'submit', 'btn btn-lg btn-block btn-outline-success', 'fechamentoDiaFiscalCaixa', 'fechamentoDiaFiscalCaixa', 'margin: 5px;', 'Fechar Dia Fiscal', '', '', '', '<i class="mdi mdi-file-check"></i>');
+                                        echo $u->retPermissaoBtn('102', 'submit', 'btn btn-lg btn-block btn-danger', 'saidaOperadorCaixa', 'saidaOperadorCaixa', 'margin: 5px;', 'Saída de Operador', '', '', '', '<i class="mdi mdi-file-check"></i>');
+                                        echo $u->retPermissaoBtn('103', 'submit', 'btn btn-lg btn-block btn-warning', 'fechamentoDiaFiscalCaixa', 'fechamentoDiaFiscalCaixa', 'margin: 5px;', 'Fechar Dia Fiscal', '', '', '', '<i class="mdi mdi-file-check"></i>');
 
                                         echo '</div>';
+                                        
                                         echo '</form>';
                                         echo '</div>';     
                                         echo '</div>';
