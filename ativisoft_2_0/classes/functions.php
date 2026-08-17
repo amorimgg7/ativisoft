4a751947feb5bd44b616571a732deb61c26fd268
amorimgg7 <amorimgg7@gmail.com>
2026-08-17 16:24:37 -0300

    20260817_3

--- ativisoft_2_0/classes/functions.php
+++ ativisoft_2_0/classes/functions.php
@@ -1,7 +1,7 @@
 <?php
  
 // Ativa a exibição de erros (útil em ambiente de desenvolvimento)
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
